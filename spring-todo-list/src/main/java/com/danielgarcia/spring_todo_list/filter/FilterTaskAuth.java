214879771463bc3b775cd5fc00cf865b9a8564f0
Daniel M. Garcia <mendoncad38@gmail.com>
2026-07-24 18:03:11 -0300

    Implementando um filtro para a criação de Tasks

---
+++ spring-todo-list/src/main/java/com/danielgarcia/spring_todo_list/filter/FilterTaskAuth.java
@@ -0,0 +1,36 @@
+package com.danielgarcia.spring_todo_list.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        var authorization = request.getHeader("Authorization");
+        var authEncoded = authorization.substring("Basic".length()).trim();
+
+        byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+        var authString = new String(authDecoded);
+        var credentials = authString.split(":");
+
+        String username = credentials[0];
+        String password = credentials[1];
+
+        System.out.println("Authorization");
+        System.out.println(username);
+        System.out.println(password);
+    }
+}
