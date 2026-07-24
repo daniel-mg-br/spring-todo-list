20bd355d5a6b2c423d7654fc4aca9736bfbfd0cd
Daniel M. Garcia <mendoncad38@gmail.com>
2026-07-24 10:15:51 -0300

    Estruturação do projeto

---
+++ spring-todo-list/src/test/java/com/danielgarcia/spring_todo_list/SpringTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielgarcia.spring_todo_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
