ca1418e3374047180e2661971efa1b6ef46243ce
Daniel M. Garcia <mendoncad38@gmail.com>
2026-07-26 16:29:09 -0300

    Finalizando o filtro e implementando o GET TASKS

--- spring-todo-list/src/main/java/com/danielgarcia/spring_todo_list/task/ITaskRepository.java
+++ spring-todo-list/src/main/java/com/danielgarcia/spring_todo_list/task/ITaskRepository.java
@@ -2,8 +2,9 @@ package com.danielgarcia.spring_todo_list.task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
-
+    List<TaskModel> findByUserId(UUID id);
 }
--- spring-todo-list/src/main/java/com/danielgarcia/spring_todo_list/task/TaskController.java
+++ spring-todo-list/src/main/java/com/danielgarcia/spring_todo_list/task/TaskController.java
@@ -1,10 +1,15 @@
 package com.danielgarcia.spring_todo_list.task;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/tasks")
@@ -14,8 +19,32 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel createTask(@RequestBody TaskModel task) {
+    public ResponseEntity createTask(@RequestBody TaskModel task, HttpServletRequest request) {
+        var userId = request.getAttribute("userId");
+        task.setUserId((UUID) userId);
+
+        var currentDate = LocalDate.now();
+        if (
+                currentDate.isAfter(ChronoLocalDate.from(task.getStartAt()))
+                ||
+                currentDate.isAfter(ChronoLocalDate.from(task.getEndAt()))
+        ) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Data de início / término inválida!");
+        }
+
+        if (task.getStartAt().isAfter(task.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de início dever ser menor que a de término!");
+        }
+
         var taskCreated = taskRepository.save(task);
-        return taskCreated;
+        return ResponseEntity.status(HttpStatus.OK).body(taskCreated);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> userTasks(HttpServletRequest request) {
+        var userId = request.getAttribute("userId");
+        return taskRepository.findByUserId((UUID) userId);
     }
 }
